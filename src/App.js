8a8e4d3e7b49ff8bc600e99910065bd7a4b8ad9a
Sowjanyaallu <allusowjanya32@gmail.com>
2023-10-10 09:40:25 +0530

    Initial commit for development branch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState,useEffect } from 'react';
+import axios  from 'axios';
 
 function App() {
+  const [posts , setPosts] = useState([])
+  const [title, setTitle] = useState('')
+  const [completed, setCompleted] = useState('')
+  const [showAll, setShowAll] = useState(false); 
+ const [numToShow, setNumToShow] = useState(5);
+
+  useEffect(()=>{
+     axios('https://jsonplaceholder.typicode.com/todos').then(resp =>{
+      setPosts(resp.data)
+     })
+  },[])
+
+  const handleInputChane = (e) =>{
+    const { name, value} = e.target;
+    if(name === 'title'){
+      setTitle(value)
+    } else if(name === 'completed')
+       setCompleted(value)
+  }
+
+  const addPost = ()=> {
+    const newPost = {
+      userId: 1,
+      Id : posts.length+1,
+      title:title,
+      completed :completed === 'true' ? true : false, 
+    }
+    setPosts([newPost,...posts])
+    setTitle('')
+    setCompleted('') 
+  }
+
+  const handleClickButton = () => {
+       setShowAll(!showAll)
+  }
+
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className='app-header'>
+        <div className='input-feild'>
+            <input type='text' className='input-box' name='title' placeholder='title' value={title} onChange={handleInputChane} />
+            <input type='text' className='input-box' name='completed' placeholder='completed true/false' value={completed}  onChange={handleInputChane} />
+        <button className='todo-button' onClick={addPost}>Add TODO</button>
+
+        </div>
+        <div className='todo-main'>
+        {posts.slice(0,showAll ? posts.length:numToShow).map((post)=> 
+         <div className= 'todo-app'key={post.id}>
+             <div className="checkbox-container">
+              <input type="checkbox" checked={post.completed} readOnly />
+            </div>
+            <div className="label-container">
+              <p>{post.title}</p>
+            </div>
+       </div>
+        )}
+        <button className='all-btn' onClick={handleClickButton}>ALL</button>
+        </div>
+
+        </header>
     </div>
   );
 }
